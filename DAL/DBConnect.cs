1bd1da72d6786f9b7e7ca4c32eaf402517d4ae76
Công Phương Đông <gongfangdong@gmail.com>
2015-11-06 21:01:02 +0700

    Upload project

---
+++ DAL/DBConnect.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DBConnect
+    {
+        public static const String CONNECTION_STRING="";
+
+        private static DBConnect instance;
+
+        private SqlConnection connection=new SqlConnection(CONNECTION_STRING);
+
+        public static DBConnect Instance
+        {
+            get {
+                if (instance == null)
+                {
+                    instance = new DBConnect();
+                }
+                return instance; }
+        }
+
+        private DBConnect();
+
+        public void connect()
+        {
+            if (connection.State == ConnectionState.Broken || connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        public void close()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ DTO/Member.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Member
+    {
+        private String id;
+
+        public String ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        
+    }
+}
