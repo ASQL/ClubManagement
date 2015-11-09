93502dcf5cc788d93c7dfbc0ad9266691ebde4c3
Công Phương Đông <gongfangdong@gmail.com>
2015-11-09 20:10:58 +0700

    Update Member

--- DTO/Member.cs
+++ DTO/Member.cs
@@ -15,6 +15,66 @@ namespace DTO
             get { return id; }
             set { id = value; }
         }
-        
+
+        private String name;
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private String job;
+
+        public String Job
+        {
+            get { return job; }
+            set { job = value; }
+        }
+
+        private DateTime birthday;
+
+        public DateTime Birthday
+        {
+            get { return birthday; }
+            set { birthday = value; }
+        }
+
+        private String email;
+
+        public String Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        private String password;
+
+        public String Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        private int isChairman;
+
+        public int IsChairman
+        {
+            get { return isChairman; }
+            set { isChairman = value; }
+        }
+
+        private byte[] image;
+
+        public byte[] Image
+        {
+            get { return image; }
+            set { image = value; }
+        }
+
+        public bool IsCorrectPassword(String password)
+        {
+            return this.password.Equals(password) ? true : false;
+        }
     }
 }
